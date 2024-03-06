33998a1bb21e6b269f930fffa3cd0d9d1d6e81b2
iammarco8 <iammarco8@gmail.com>
2024-03-06 14:26:42 -0500

    this is a backup remember: 'git log --> copy sha ---> git checkout sha'

---
+++ rout/homepage.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import {
+    getAllMenu, getSingleOrder, 
+}
+// should be able to see the full menu
+// see the list of all food items to create an order
+// see own orders
+// 
--- rout/login.js
+++ rout/login.js
@@ -1,2 +1,3 @@
 import express from "express";
 import { adminUserExists, isLoginCorrect } from "../database/database.js";
+// 
